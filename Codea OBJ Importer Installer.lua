7b1f992aa13cc9824419b219e6c210b9b995950d
Puffin & Turtle Software <puffinandturtle@gmail.com>
2015-10-13 17:34:11 +0100

    add anti-aliasing to wireframe mode

--- Codea OBJ Importer Installer.lua
+++ Codea OBJ Importer Installer.lua
@@ -648,11 +648,11 @@ void main(void) {
     highp vec4 col = vColor;
     if (!gl_FrontFacing) col.rgb *= 0.5; //darken rear-facing struts
     highp vec3 d = fwidth(vNormal);    
-    highp vec3 tdist = smoothstep(vec3(0.0), d * 2., vNormal); //thicken line by multiplying d
+    highp vec3 tdist = smoothstep(vec3(0.0), d * 3., vNormal); //thicken line by multiplying d
 
     //2 methods: 1. discard method: best way of ensuring back facing struts show through
     if (min(min(tdist.x, tdist.y), tdist.z) > 0.5) discard; 
-    else gl_FragColor = col;
+    else gl_FragColor = mix(col, vec4(0.), 2. * min(min(tdist.x, tdist.y), tdist.z)); // anti-aliasing
     
     //2. alpha method means some rear faces wont show. Would be good for a "solid" mode though
     //gl_FragColor = mix(col, vec4(0.), min(min(tdist.x, tdist.y), tdist.z)); 
